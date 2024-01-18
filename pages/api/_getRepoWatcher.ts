f94167356399e8c3a4d8ad299f691eab69654e15
JYC0413 <juyichen0413@foxmail.com>
2024-01-18 11:59:34 +0800

    fix

--- pages/api/_getRepoWatcher.ts
+++ pages/api/_getRepoWatcher.ts
@@ -1,4 +1,6 @@
+// @ts-ignore
 export default async function (req, res) {
+    // @ts-ignore
     const {url,token} = req.body;
     const response = await fetch(`https://code.flows.network/webhook/vIognrnNfVdQSlIRIyIM?owner_repo=${url}&token=${token}`)
     const data = await response.text()
